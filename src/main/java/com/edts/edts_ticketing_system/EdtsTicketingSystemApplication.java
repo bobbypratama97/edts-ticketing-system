e8370cabccc9c7ed9dd7a914c95a7aa9d3df53ef
Bobby Pratama <bpratama77@gmail.com>
2026-08-19 19:30:25 +0700

    feat: initial spring boot project setup

---
+++ src/main/java/com/edts/edts_ticketing_system/EdtsTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.edts.edts_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtsTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtsTicketingSystemApplication.class, args);
+	}
+
+}
